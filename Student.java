180fe643e56bc13be9cd853e5e5974fbd0ac269a
KunalTanwar130 <kunaldropshipping13@gmail.com>
2025-10-30 14:27:44 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String course;
+    private double fee;
+
+    // Getters and setters
+}
